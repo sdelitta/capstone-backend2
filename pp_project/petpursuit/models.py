1e26eb5b375fb25e67a736d6876721de2347fa72
Steven DeLitta <sdelitta@gmail.com>
2022-05-06 19:25:32 -0400

    Finished models and made initial migration

--- pp_project/petpursuit/models.py
+++ pp_project/petpursuit/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    stateName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.stateName
+
+class Shelter(models.Model):
+    state = models.ForeignKey(State, 
+    on_delete=models.CASCADE, 
+    related_name='shelters')
+    shelterName = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    website = models.TextField()
+
+    def __str__(self):
+        return self.shelterName
+
+class Canine(models.Model):
+    shelter = models.ForeignKey(Shelter, 
+    on_delete=models.CASCADE, 
+    related_name='canines')
+    dogName = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.TextField()
+    location = models.TextField()
+
+    def __str__(self):
+        return self.dogName
+
+class Feline(models.Model):
+    shelter = models.ForeignKey(Shelter, 
+    on_delete=models.CASCADE, 
+    related_name='felines')
+    catName = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.TextField()
+    location = models.TextField()
+
+    def __str__(self):
+        return self.catName
+
+class User(models.Model):
+    userName = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.TextField()
+
+    def __str__(self):
+        return self.userName
